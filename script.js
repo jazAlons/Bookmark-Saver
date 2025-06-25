7037d58aab0dd8fa9f6a1cd925fc1f13fb204660
jazAlonso <jazminalonsobri@gmail.com>
2025-06-25 08:20:58 -0600

    Trim input values before processing bookmarks

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const addBookmarkBtn = document.getElementById("add-bookmark");
 const bookmarkList = document.getElementById("bookmark-list");
 const bookmarkNameInput = document.getElementById("bookmark-name");
-const bookmarkUrlInput = document.getElementById("add-bookmark");
+const bookmarkUrlInput = document.getElementById("bookmark-url");
+
+document.addEventListener("DOMContentLoaded", loadBookmarks)
+
+addBookmarkBtn.addEventListener("click", function () {
+
+    // Retrieve the user input values for bookmark name and URL, removing any extra spaces
+    const name = bookmarkNameInput.value.trim()
+    const url = bookmarkUrlInput.value.trim()
+
+
+})
